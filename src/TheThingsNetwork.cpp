3326813342f6439198fc7740b62397c270658492
danalvarez <d.alvrzx@gmail.com>
2025-09-16 12:00:27 -0600

    Changed pgm_read_dword to pgm_read_ptr so it works in both AVR and ESP32

--- src/TheThingsNetwork.cpp
+++ src/TheThingsNetwork.cpp
@@ -380,12 +380,12 @@ int pgmstrcmp(const char *str1, uint8_t str2Index, uint8_t table = CMP_TABLE)
 
   switch (table) {
   case CMP_ERR_TABLE:
-    strcpy_P(str2, (char *)pgm_read_dword(&(compareerr_table[str2Index])));
+    strcpy_P(str2, (char *)pgm_read_ptr(&(compareerr_table[str2Index])));
     break;
 
   default:
   case CMP_TABLE:
-    strcpy_P(str2, (char *)pgm_read_dword(&(compare_table[str2Index])));
+    strcpy_P(str2, (char *)pgm_read_ptr(&(compare_table[str2Index])));
   }
 
   return memcmp(str1, str2, min(strlen(str1), strlen(str2)));
@@ -603,7 +603,7 @@ ttn_response_code_t TheThingsNetwork::getLastError(){
 void TheThingsNetwork::debugPrintIndex(uint8_t index, const char *value)
 {
   char message[100];
-  strcpy_P(message, (char *)pgm_read_dword(&(show_table[index])));
+  strcpy_P(message, (char *)pgm_read_ptr(&(show_table[index])));
   debugPrint(message);
   if (value)
   {
@@ -617,10 +617,10 @@ void TheThingsNetwork::debugPrintMessage(uint8_t type, uint8_t index, const char
   switch (type)
   {
   case ERR_MESSAGE:
-    strcpy_P(message, (char *)pgm_read_dword(&(error_msg[index])));
+    strcpy_P(message, (char *)pgm_read_ptr(&(error_msg[index])));
     break;
   case SUCCESS_MESSAGE:
-    strcpy_P(message, (char *)pgm_read_dword(&(success_msg[index])));
+    strcpy_P(message, (char *)pgm_read_ptr(&(success_msg[index])));
     break;
   }
   debugPrint(message);
@@ -1459,28 +1459,28 @@ void TheThingsNetwork::sendCommand(uint8_t table, uint8_t index, bool appendSpac
   switch (table)
   {
   case MAC_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_table[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_table[index])));
     break;
   case MAC_GET_SET_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_options[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_options[index])));
     break;
   case MAC_JOIN_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_join_mode[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_join_mode[index])));
     break;
   case MAC_CH_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_ch_options[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_ch_options[index])));
     break;
   case MAC_TX_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_tx_table[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_tx_table[index])));
     break;
   case SYS_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(sys_table[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(sys_table[index])));
     break;
   case RADIO_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(radio_table[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(radio_table[index])));
     break;
   case MAC_RESET_TABLE:
-    strcpy_P(command, (char *)pgm_read_dword(&(mac_reset_table[index])));
+    strcpy_P(command, (char *)pgm_read_ptr(&(mac_reset_table[index])));
     break;
   default:
     return;
